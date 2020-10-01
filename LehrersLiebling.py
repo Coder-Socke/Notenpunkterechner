fe8070497220ce72b59f6f2d6a06244aad5e965c
Coder-Socke <71214455+Coder-Socke@users.noreply.github.com>
2020-10-01 13:43:59 +0200

    Add files via upload

---
+++ LehrersLiebling.py
@@ -0,0 +1,44 @@
+import math
+
+RP = float(input("Anzahl RP: "))
+GP = float(input("Anzahl GP: "))
+p = RP/GP*100
+if RP > GP:
+    n = ('Error: RP-Anzahl ist höher als GP-Anzahl')
+    p = RP/GP*100
+elif p >= 95:
+    n = 15
+elif p >= 90:
+    n = 14
+elif p >= 85:
+    n = 13
+elif p >= 80:
+    n = 12
+elif p >= 75:
+    n = 11
+elif p >= 70:
+    n = 10
+elif p >= 65:
+    n = 9
+elif p >= 60:
+    n = 8
+elif p >= 55:
+    n = 7
+elif p >= 50:
+    n = 6
+elif p >= 45:
+    n = 5
+elif p >= 39:
+    n = 4
+elif p >= 33:
+    n = 3
+elif p >= 27:
+    n = 2
+elif p >= 20:
+    n = 1
+elif p < 20 and p >= 0:
+    n = 0
+else:
+    n = 'ungültig'
+
+print(n)
